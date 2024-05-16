1c95a61432becbd5a07041f3f49dc52c3071e9b5
Giorgi Maisuradze <giorgimaisuradze2711@gmail.com>
2024-05-16 16:17:14 +0400

    Fixed addTableItem() function

--- src/CustomTable.svelte
+++ src/CustomTable.svelte
@@ -1,8 +1,8 @@
-<svelte:options tag = "custom-element"/>
+<svelte:options tag="custom-table"/>
 
 <script>
-	export let tableItems= [];
-	let tableColCount = 4;
+	export let tableItems = [];
+    let tableColCount = 4;
     $: tableRowCount = Math.ceil(tableItems.length / tableColCount);
 
 	export function addTableItem(tableItemObject) {
@@ -11,6 +11,7 @@
         }
 	}
 </script>
+
 
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
--- src/main.js
+++ src/main.js
@@ -1,10 +1,1 @@
 import CustomTable from './CustomTable.svelte';
-
-const customTable = new CustomTable({
-	target: document.body,
-	props: {
-		tableItems: ["ItemOne","ItemTwo","ItemThree"]
-	}
-});
-
-export default customTable;
