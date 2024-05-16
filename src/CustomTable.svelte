e28675d196050c09baa7d36d98bc1183fda1282c
Giorgi Maisuradze <giorgimaisuradze2711@gmail.com>
2024-05-16 15:03:43 +0400

    Added styles on custom table element

--- src/CustomTable.svelte
+++ src/CustomTable.svelte
@@ -5,6 +5,35 @@
 </script>
 
 <style>
+	@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+	
+	table{
+		font-family: "Poppins", sans-serif;
+		color: #FFF;
+		font-weight: bold;
+
+		padding: 5px;
+		border-spacing: 5px;
+
+		border: 2px solid #999;
+		border-radius: 5px;
+	}
+
+	tr{
+
+	}
+
+	td{
+		text-align: center;
+
+		width: 100px;
+		height: 100px;
+
+		background-color: #999;
+
+		border: none;
+		border-radius: 5px;
+	}
 </style>
 
 <table>
